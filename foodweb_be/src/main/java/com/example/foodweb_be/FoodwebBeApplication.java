ad29ef8a3d4ca9b5f304b32b98518dc700a45679
phuyen27 <puyen274@gmail.com>
2026-04-02 22:49:22 +0700

    first commit

---
+++ foodweb_be/src/main/java/com/example/foodweb_be/FoodwebBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodweb_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodwebBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodwebBeApplication.class, args);
+	}
+
+}
